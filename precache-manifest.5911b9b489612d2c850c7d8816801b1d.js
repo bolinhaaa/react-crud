46ee66e6377b129fbf2395c8609687fd601d4b7c
Bolinha <jonatabueno@live.com>
2019-12-27 02:03:40 -0300

    Primeiro Commit

---
+++ precache-manifest.5911b9b489612d2c850c7d8816801b1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a296d62c0f0c95aca58a01860c093d06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c77674bcde7df0bb23c",
+    "url": "/static/css/main.dffd8c07.chunk.css"
+  },
+  {
+    "revision": "ad0140a091e8031bca9b",
+    "url": "/static/js/2.6915b869.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.6915b869.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6c77674bcde7df0bb23c",
+    "url": "/static/js/main.9f5faf11.chunk.js"
+  },
+  {
+    "revision": "c8202c9f40dfee5a63ab",
+    "url": "/static/js/runtime-main.abf03639.js"
+  }
+]);
